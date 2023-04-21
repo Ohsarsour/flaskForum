3f6c0718e012878b9930e5f7886dc6bf6909a6a5
Ohsarsour <obaidullahsarsour@gmail.com>
2023-04-21 11:09:42 -0400

    minor mistake fixes

--- flaskblog/forms.py
+++ flaskblog/forms.py
@@ -38,3 +38,4 @@ class PostForm(FlaskForm):
     title = StringField('Title', validators=[DataRequired()])
     content = TextAreaField('Content', validators=[DataRequired()])
     submit = SubmitField('Post')
+
